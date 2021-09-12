1ff8891327a1c07b93737c6ad8a9f8b0ea05bc39
xiaozou <1424113865@qq.com>
2021-09-12 09:10:54 +0800

    全局组件 <Nav>

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
         statistics.vue
+        <Nav></Nav>
     </div>
 </template>
 
 <script lang="ts">
     export default ({
-        
+        name:'Statistics',
     })
 </script>
 
