25061afafb8bd92f78d93c3b25dd5a5b478b41e1
xiaozou <1424113865@qq.com>
2021-09-15 14:46:34 +0800

    完成 tag

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tags">
         <div class="new">
-            <button>新增标签</button>
+            <button @click="create">新增标签</button>
         </div>
         <ul class="current">
             <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)"
@@ -16,7 +16,7 @@
 
     @Component
     export default class Tags extends Vue{
-        @Prop() dataSource:string[] | undefined;
+        @Prop(Array) readonly dataSource:string[] | undefined;
         selectedTags:string[] = [];
         toggle(tag:string){
             const index = this.selectedTags.indexOf(tag);
@@ -24,6 +24,14 @@
                 this.selectedTags.splice(index,1);
             }else{
                 this.selectedTags.push(tag);
+            }
+        }
+        create(){
+            const name = window.prompt('请输入标签名');
+            if(name === ''){
+                window.alert('标签名不能为空');
+            }else if(this.dataSource){
+                this.$emit('update:dataSource',[...this.dataSource,name]);
             }
         }
     }
@@ -42,11 +50,13 @@
             > li{
                 $bg:#d9d9d9;
                 background: $bg;
-                height: 24px;
-                line-height: 24px;
-                border-radius: (24px/2);
+                $h: 24px;
+                height: $h;
+                line-height: $h;
+                border-radius: $h/2;
                 padding: 0 16px;
                 margin-right: 12px;
+                margin-top: 4px;
                 &.selected{
                     background: darken($bg,50%);
                     color: white;
