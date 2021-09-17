85a9ba122660f72442d1578af6fdfec8f098fcf1
xiaozou <1424113865@qq.com>
2021-09-17 14:51:04 +0800

    展示 tag 的 name

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -10,6 +10,7 @@
 <script lang="ts">
     import Vue from 'vue';
     import { Component, Prop, Watch } from 'vue-property-decorator';
+    
     @Component
     export default class FormItem extends Vue{
         @Prop({default:''}) readonly value!: string;
