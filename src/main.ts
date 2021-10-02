5f0c5faf81cb030e952ecabaad2ea289ff2cccfa
xiaozou <1424113865@qq.com>
2021-10-03 00:28:00 +0800

    去除二维码

--- src/main.ts
+++ src/main.ts
@@ -27,8 +27,8 @@ window.onload = function (){
   },0);
 }
 
-// if(document.documentElement.clientHeight){
-//   window.alert('请使用手机打开本页面，以保证浏览效果');
+if(document.documentElement.clientHeight){
+  window.alert('请使用手机打开本页面，以保证浏览效果');
 //   const img = document.createElement('img');
 //   img.src = './qrcode.png';
 //   img.style.position = 'fixed';
@@ -37,4 +37,4 @@ window.onload = function (){
 //   img.style.transform = 'translate(-50%,-50%)';
 //   img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
 //   document.body.appendChild(img);
-// }
+}
