47fb09650a90e884c251f070c095dcd0ea9ef958
xiaozou <1424113865@qq.com>
2021-12-10 17:04:56 +0800

    update

---
+++ dist/precache-manifest.98c3be4703397344574d77b033ca7a7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ce02454431af472e39",
+    "url": "/xuexue-money-website/css/app.1b6ef201.css"
+  },
+  {
+    "revision": "78ba1c40cfbd5cab7ffd",
+    "url": "/xuexue-money-website/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/xuexue-money-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/xuexue-money-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6668d352884d40772ba08ffae42ba191",
+    "url": "/xuexue-money-website/index.html"
+  },
+  {
+    "revision": "b5ce02454431af472e39",
+    "url": "/xuexue-money-website/js/app.2b3fcfeb.js"
+  },
+  {
+    "revision": "78ba1c40cfbd5cab7ffd",
+    "url": "/xuexue-money-website/js/chunk-vendors.718336bc.js"
+  },
+  {
+    "revision": "dabf655311b1408d2798f4f58c7fca7e",
+    "url": "/xuexue-money-website/manifest.json"
+  },
+  {
+    "revision": "278e32771c83a6f6c0d6720edf1961fd",
+    "url": "/xuexue-money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xuexue-money-website/robots.txt"
+  }
+]);
